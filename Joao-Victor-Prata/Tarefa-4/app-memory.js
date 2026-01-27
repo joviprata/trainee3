82bf1a0716d848b46f0f0cf38c882ee6dc7f8fcc
joviprata <joviprata07@gmail.com>
2026-01-27 17:39:10 -0300

    UPDATE, DELETE e ajustes

--- Joao-Victor-Prata/Tarefa-4/app-memory.js
+++ Joao-Victor-Prata/Tarefa-4/app-memory.js
@@ -4,28 +4,25 @@ const app = express();
 const port = 3000;
 
 let usuarios = [
-    { id: 1, nome: 'Péricles', email: 'alice@gmail.com'},
-    { id: 2, nome: 'Reinaldo', email: 'bob@gmail.com'},
-    { id: 3, nome: 'Eclair', email: ''},
-    { id: 4, nome: 'Jurandir', email: ''},
-    { id: 5, nome: 'Epaminondas', email: ''},
-    { id: 6, nome: 'Geremias', email: ''},
+    { id: 1, nome: 'Péricles', email: 'pericles@gmail.com'},
+    { id: 2, nome: 'Reinaldo', email: 'reinaldo@gmail.com'},
+    { id: 3, nome: 'Gertrudes', email: 'gertrudes@gmail.com'},
+    { id: 4, nome: 'Eclair', email: 'eclair@gmail.com'},
+    { id: 5, nome: 'Jurandir', email: 'jurandir@gmail.com'},
+    { id: 6, nome: 'Epaminondas', email: 'epaminondas@gmail.com'},
+    { id: 7, nome: 'Geremias', email: 'geremias@gmail.com'},
+    { id: 8, nome: 'Adelaide', email: 'adelaide@gmail.com'},
 ];
 
-app.use( express.json() ); // Middleware que traduz o body do request para JSON
-
-
-app.get('/usuarios', (req, res) => {
-    res.status(200);
-    res.send(usuarios); // Ler informações sobre todos os usuários (antes das alterações)
-})
+app.use(express.json()); // Middleware que traduz o body do request para JSON
 
 
 app.post('/usuarios', (req, res) => { // CREATE
+    // Criar um usuário novo informando nome e email:
     const { nome, email } = req.body;
 
     if (!nome || !email) {
-        res.status(400).send({error: "Nome e e-mail são obrigatórios"});
+        return res.status(400).send({error: "Nome e e-mail são obrigatórios"});
     };
 
     const novoUsuario = {
@@ -36,30 +33,61 @@ app.post('/usuarios', (req, res) => { // CREATE
 
     usuarios.push(novoUsuario);
 
-    res.status(201).send('Novo usuário adicionado:');
-    res.send(novoUsuario);
-
+    return res.status(201).send({message: 'Novo usuário adicionado', usuario: novoUsuario});
 });
 
+
+app.get('/usuarios', (req, res) => { // READ
+    res.status(200).send(usuarios); // Ler informações sobre todos os usuários
+})
+
+
 app.get('/usuarios/:id', (req, res) => { // READ
-    // Pegar informações do usuário de id específico no array usuarios
+    // Ler informações sobre usuário com id específico:
     const { id } = req.params; 
     const usuarioSelecionado = usuarios.find(usuario => usuario.id === Number(id));
     
     if (usuarioSelecionado) {
-        res.status(200).send(usuarioSelecionado); 
-    }
-    else {
-        res.status(404).send({error: "Usuário não encontrado"});
+        return res.status(200).send(usuarioSelecionado); 
     }
 
+    return res.status(404).send({error: "Usuário não encontrado"});
 });
 
 
-app.get('/usuarios', (req, res) => {
-    res.status(200);
-    res.send(usuarios); // Ler informações sobre todos os usuários (após as alterações)
-})
+app.patch('/usuarios/:id', (req, res) => { // UPDATE
+    // Procurar usuário pelo id:
+    const { id } = req.params; 
+    const usuarioSelecionado = usuarios.find(usuario => usuario.id === Number(id));
+    
+    if (!usuarioSelecionado) {
+        return res.status(404).send({error: "Usuário não encontrado"});
+    }
+
+    // Atualizar dados:
+    const { nome, email } = req.body;
+
+    if (nome) usuarioSelecionado.nome = nome;
+    if (email) usuarioSelecionado.email = email;
+
+    return res.status(200).send({message: 'Dados de usuário foram atualizados', usuario: usuarioSelecionado});
+});
+
+
+app.delete('/usuarios/:id', (req, res) => { // DELETE
+    // Procurar usuário pelo id:
+    const { id } = req.params; 
+    const usuarioIndex = usuarios.findIndex(usuario => usuario.id === Number(id));
+    
+    if (i_usuario_a_deletar === -1) {
+        return res.status(404).send({error: "Usuário não encontrado"});
+    }
+
+    // Deletar usuário do array:
+    const usuarioDeletado = usuarios.splice(usuarioIndex, 1)[0]; 
+
+    return res.status(200).send({message: 'Usuário foi deletado', usuario: usuarioDeletado});
+});
 
 
 app.listen(port, () => { // escuta na porta 3000
