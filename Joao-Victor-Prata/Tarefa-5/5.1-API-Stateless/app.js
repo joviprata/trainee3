87772b1ff334fa4585db547871515d8cb30ab0b4
joviprata <joviprata07@gmail.com>
2026-02-02 16:24:52 -0300

    autenticação stateless

--- Joao-Victor-Prata/Tarefa-5/5.1-API-Stateless/app.js
+++ Joao-Victor-Prata/Tarefa-5/5.1-API-Stateless/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import 'dotenv/config';
 
 const app = express();
 const port = 3000;
 
-let usuarios = [
+const usuarios = {
+  [process.env.TOKEN_USUARIO_ADMIN_1]: { id: 1, nome: 'Jovi' }
+}
+
+let clientes = [
     { id: 1, nome: 'Péricles', email: 'pericles@gmail.com'},
     { id: 2, nome: 'Reinaldo', email: 'reinaldo@gmail.com'},
     { id: 3, nome: 'Gertrudes', email: 'gertrudes@gmail.com'},
@@ -14,54 +19,77 @@ let usuarios = [
     { id: 8, nome: 'Adelaide', email: 'adelaide@gmail.com'},
 ];
 
+
+// Autenticação:
+function authUsuario(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).send({error: "Usuário não autenticado: faltando credenciais"});
+    };
+
+    const token = authHeader.replace('ApiKey ', '');
+
+    const user = usuarios[token];
+
+    if (!user) {
+        return res.status(401).send({error: "Usuário não encontrado"});
+    };
+
+    req.user = user; // pega dados de usuário (id e nome) se necessário obter depois nos endpoints
+
+    next();
+};
+
+
 app.use(express.json()); // Middleware que traduz o body do request para JSON
 
 
-app.post('/usuarios', (req, res) => { // CREATE
-    // Criar um usuário novo informando nome e email:
+app.post('/clientes', authUsuario, (req, res) => { // CREATE
+    // Criar um cliente novo informando nome e email:
     const { nome, email } = req.body;
 
     if (!nome || !email) {
         return res.status(400).send({error: "Nome e e-mail são obrigatórios"});
     };
 
-    const novoUsuario = {
-        id: usuarios.length + 1,
+    const novoCliente = {
+        id: clientes.length + 1,
         nome,
         email
     };
 
-    usuarios.push(novoUsuario);
+    clientes.push(novoCliente);
 
-    return res.status(201).send({message: 'Novo usuário adicionado', usuario: novoUsuario});
+    return res.status(201).send({message: 'Novo cliente adicionado', cliente: novoCliente});
 });
 
 
-app.get('/usuarios', (req, res) => { // READ
-    res.status(200).send(usuarios); // Ler informações sobre todos os usuários
+app.get('/clientes', authUsuario, (req, res) => { // READ
+    res.status(200).send(clientes); // Ler informações sobre todos os clientes
 });
 
 
-app.get('/usuarios/:id', (req, res) => { // READ
-    // Ler informações sobre usuário com id específico:
+app.get('/clientes/:id', authUsuario, (req, res) => { // READ
+    // Ler informações sobre cliente com id específico:
     const { id } = req.params; 
-    const usuarioSelecionado = usuarios.find(usuario => usuario.id === Number(id));
+    const clienteSelecionado = clientes.find(cliente => cliente.id === Number(id));
     
-    if (!usuarioSelecionado) {
-        return res.status(404).send({error: "Usuário não encontrado"});
+    if (!clienteSelecionado) {
+        return res.status(404).send({error: "Cliente não encontrado"});
     };
 
-    return res.status(200).send(usuarioSelecionado);
+    return res.status(200).send(clienteSelecionado);
 });
 
 
-app.put('/usuarios/:id', (req, res) => { // UPDATE
-    // Procurar usuário pelo id:
+app.put('/clientes/:id', authUsuario, (req, res) => { // UPDATE
+    // Procurar cliente pelo id:
     const { id } = req.params; 
-    const usuarioSelecionado = usuarios.find(usuario => usuario.id === Number(id));
+    const clienteSelecionado = clientes.find(cliente => cliente.id === Number(id));
     
-    if (!usuarioSelecionado) {
-        return res.status(404).send({error: "Usuário não encontrado"});
+    if (!clienteSelecionado) {
+        return res.status(404).send({error: "Cliente não encontrado"});
     };
 
     // Atualizar todos os dados:
@@ -71,45 +99,45 @@ app.put('/usuarios/:id', (req, res) => { // UPDATE
         return res.status(400).send({error: "Nome e e-mail são obrigatórios"});
     };
 
-    usuarioSelecionado.nome = nome;
-    usuarioSelecionado.email = email;
+    clienteSelecionado.nome = nome;
+    clienteSelecionado.email = email;
 
-    return res.status(200).send({message: 'Dados de usuário foram atualizados', usuario: usuarioSelecionado});
+    return res.status(200).send({message: 'Dados de cliente foram atualizados', cliente: clienteSelecionado});
 });
 
 
-app.patch('/usuarios/:id', (req, res) => { // UPDATE
-    // Procurar usuário pelo id:
+app.patch('/clientes/:id', authUsuario, (req, res) => { // UPDATE
+    // Procurar cliente pelo id:
     const { id } = req.params; 
-    const usuarioSelecionado = usuarios.find(usuario => usuario.id === Number(id));
+    const clienteSelecionado = clientes.find(cliente => cliente.id === Number(id));
     
-    if (!usuarioSelecionado) {
-        return res.status(404).send({error: "Usuário não encontrado"});
+    if (!clienteSelecionado) {
+        return res.status(404).send({error: "Cliente não encontrado"});
     };
 
     // Atualizar um ou mais dados:
     const { nome, email } = req.body;
 
-    if (nome) usuarioSelecionado.nome = nome;
-    if (email) usuarioSelecionado.email = email;
+    if (nome) clienteSelecionado.nome = nome;
+    if (email) clienteSelecionado.email = email;
 
-    return res.status(200).send({message: 'Dados de usuário foram atualizados', usuario: usuarioSelecionado});
+    return res.status(200).send({message: 'Dados de cliente foram atualizados', cliente: clienteSelecionado});
 });
 
 
-app.delete('/usuarios/:id', (req, res) => { // DELETE
-    // Procurar usuário pelo id:
+app.delete('/clientes/:id', authUsuario, (req, res) => { // DELETE
+    // Procurar cliente pelo id:
     const { id } = req.params; 
-    const usuarioIndex = usuarios.findIndex(usuario => usuario.id === Number(id));
+    const clienteIndex = clientes.findIndex(cliente => cliente.id === Number(id));
     
-    if (usuarioIndex === -1) {
-        return res.status(404).send({error: "Usuário não encontrado"});
+    if (clienteIndex === -1) {
+        return res.status(404).send({error: "Cliente não encontrado"});
     };
 
-    // Deletar usuário do array:
-    const usuarioDeletado = usuarios.splice(usuarioIndex, 1)[0]; 
+    // Deletar cliente do array:
+    const clienteDeletado = clientes.splice(clienteIndex, 1)[0]; 
 
-    return res.status(200).send({message: 'Usuário foi deletado', usuario: usuarioDeletado});
+    return res.status(200).send({message: 'Cliente foi deletado', cliente: clienteDeletado});
 });
 
 
