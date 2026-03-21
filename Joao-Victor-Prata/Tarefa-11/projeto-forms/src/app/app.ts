ac23f6ec7b48add77458dff76605816936e7b760
joviprata <joviprata07@gmail.com>
2026-03-21 17:10:28 -0300

    ajustes css e adicionado header

--- Joao-Victor-Prata/Tarefa-11/projeto-forms/src/app/app.ts
+++ Joao-Victor-Prata/Tarefa-11/projeto-forms/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserForm } from './user-form/user-form';
+import { Header } from "./header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,  UserForm],
+  imports: [RouterOutlet, UserForm, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
