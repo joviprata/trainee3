cc52ece40f06f81f5620fa309c8f180dcc9daa7c
joviprata <joviprata07@gmail.com>
2026-03-25 20:48:25 -0300

    criação de formulároio ReactiveForms

--- Joao-Victor-Prata/Tarefa-12/projeto-forms/src/app/app.ts
+++ Joao-Victor-Prata/Tarefa-12/projeto-forms/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { UserForm } from './user-form/user-form';
 import { Header } from "./header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserForm, Header],
+  imports: [UserForm, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- Joao-Victor-Prata/Tarefa-12/projeto-forms/src/app/user-form/user-form.ts
+++ Joao-Victor-Prata/Tarefa-12/projeto-forms/src/app/user-form/user-form.ts
@@ -1,13 +1,51 @@
-import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component, computed } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, Validators, FormArray, FormControl } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-user-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './user-form.html',
   styleUrl: './user-form.css',
 })
+
 export class UserForm {
-  dadosSalvos = false;
+  userForm = new FormGroup({
+    nome: new FormControl('', {
+      validators: [ Validators.required, Validators.maxLength(255) ]
+    }),
+    email: new FormControl('', {
+      validators: [ Validators.required, Validators.email, Validators.maxLength(255) ]
+    })
+    
+  });
+
+  get nameIsInvalid() {
+    return (
+      this.userForm.controls.nome.touched &&
+      this.userForm.controls.nome.invalid
+    )
+  }
+
+  get emailIsInvalid() {
+    return (
+      this.userForm.controls.email.touched &&
+      this.userForm.controls.email.dirty &&
+      this.userForm.controls.email.invalid
+    );
+  }
+
+  savedUserData: any = null;
+
+  onSubmit() {
+    this.savedUserData = this.userForm.value;
+    console.log(this.savedUserData)
+  }
+
+  onReset() {
+    this.savedUserData = null;
+    this.userForm.reset();
+  }
+  
 }
