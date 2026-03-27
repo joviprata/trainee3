02d7999f001bf896f975263ea0786679ccc87c0b
joviprata <joviprata07@gmail.com>
2026-03-27 17:00:51 -0300

    instalação do bootstrap

--- Joao-Victor-Prata/Tarefa-13/projeto-crud/frontend/src/app/app.ts
+++ Joao-Victor-Prata/Tarefa-13/projeto-crud/frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Toolbar } from "./toolbar/toolbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Toolbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
