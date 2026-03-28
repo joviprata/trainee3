ec531f826f239a8f1b16d5c65a018da4a136c316
joviprata <joviprata07@gmail.com>
2026-03-27 21:07:18 -0300

    routing

--- Joao-Victor-Prata/Tarefa-13/projeto-crud/frontend/src/app/app.routes.ts
+++ Joao-Victor-Prata/Tarefa-13/projeto-crud/frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { StudyList } from './study-list/study-list';
+import { StudyCreate } from './study-create/study-create';
+import { StudyEdit } from './study-edit/study-edit';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: StudyList,
+  },
+  {
+    path: 'new',
+    component: StudyCreate,
+  },
+  {
+    path: ':id/edit',
+    component: StudyEdit,
+  }
+];
--- Joao-Victor-Prata/Tarefa-13/projeto-crud/frontend/src/app/toolbar/toolbar.ts
+++ Joao-Victor-Prata/Tarefa-13/projeto-crud/frontend/src/app/toolbar/toolbar.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './toolbar.html',
   styleUrl: './toolbar.scss',
 })
