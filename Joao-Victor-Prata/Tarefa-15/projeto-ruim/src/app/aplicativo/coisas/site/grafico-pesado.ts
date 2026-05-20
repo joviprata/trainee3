25d76ae38a3c11f964d754c6970d7adcecb1e7c4
joviprata <joviprata07@gmail.com>
2026-05-19 21:35:35 -0300

    envio de arquivos do projeto

---
+++ Joao-Victor-Prata/Tarefa-15/projeto-ruim/src/app/aplicativo/coisas/site/grafico-pesado.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grafico-pesado',
+  imports: [],
+  templateUrl: './grafico-pesado.html',
+  styleUrl: './grafico-pesado.css',
+})
+export class GraficoPesado {
+
+}
